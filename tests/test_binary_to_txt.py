f2c2191905292c3826b174ab66f4a755e3261d4d
matheus <geof.matheus@gmail.com>
2022-04-06 18:18:02 -0300

    update test_binary_to_txt.py

--- tests/test_binary_to_txt.py
+++ tests/test_binary_to_txt.py
@@ -4,8 +4,8 @@ from file_binary import binary_to_txt
 
 def test_bin_to_txt():
     binary_to_txt.bin_to_txt("bin_matrix_test.bin", 2)
-    matrix_exist = exists("bin_matrix_test.txt")
     binary_to_txt.bin_to_txt("bin_array_test.bin", 2)
+    matrix_exist = exists("bin_matrix_test.txt")
     array_exist = exists("bin_array_test.txt")
     assert array_exist == True
     assert matrix_exist == True
