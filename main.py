c0041f6288f1a6af587314b219d0c8dbaf1c60fe
matheus <geof.matheus@gmail.com>
2022-04-07 10:35:57 -0300

    added main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+from file_binary import binary_average_file
+from file_binary import binary_array_file
 from file_binary import binary_matrix_file
 from file_binary import binary_to_txt
-
-binary_matrix_file.create_binary_matrix_file("matriz.bin", 4, 4)
-binary_to_txt.bin_to_txt("matriz.bin", 4)
